d657129f9492388d43ec5c2829d6ca7651689d62
Nolen Lawton <mnlawton@icloud.com>
2022-12-15 21:38:47 -0600

    consoled animals using its array index

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -30,18 +30,20 @@ console.log('Number of favorite foods: ', foodArray.length);
 console.log('--- 3. Accessing items in an array ---');
 
 // Example: Log the first animal from the array using it's array index
-console.log('First animal is', animalArray[0])
+console.log('First animal is', animalArray[0]);
 
 // 3.a. TODO: Log the second animal in the array 
 
+console.log('The second animal is', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
 
+console.log('The last animal is', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
 
-
+console.log('The last animal is', animalArray[animalArray.length - 1]);
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
