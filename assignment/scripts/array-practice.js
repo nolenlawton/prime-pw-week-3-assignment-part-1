3f116b9a41eff7896c2a2457bb52e52bad70d86b
Nolen Lawton <mnlawton@icloud.com>
2022-12-16 12:07:49 -0600

    finished stretch goals

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -100,14 +100,21 @@ console.log('Changed second item,', foodArray);
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
 
-
+foodArray.sort();
+foodArray.reverse();
+console.log(foodArray);
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+
+console.log(foodArray.join(' and '));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+
+foodAndAnimalArray = foodArray.concat(animalArray);
+console.log(foodAndAnimalArray);
